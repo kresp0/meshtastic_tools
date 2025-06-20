5920a777e8c5b42466b264f15c014893c31eafa9
Santiago Crespo <420430+kresp0@users.noreply.github.com>
2025-06-21 00:45:55 +0200

    Add files via upload

---
+++ read_messages_from_id.py
@@ -0,0 +1,51 @@
+import time
+import sys
+import threading
+from pubsub import pub
+from meshtastic.serial_interface import SerialInterface
+
+serial_port = '/dev/ttyUSB0'
+last_message_time = time.time()
+stop_timeout = 0
+done = threading.Event()
+
+FILTER_ID = '12345678'
+
+def handle_text(packet, interface):
+    global last_message_time
+    text = packet['decoded'].get('text') or \
+           packet['decoded'].get('payload', b'').decode('utf-8', 'ignore')
+    src = packet.get('fromId')
+    if src and src.lstrip('!') == FILTER_ID and text:
+#        print(f"{src}: {text}")
+        print(f"{text}")
+        last_message_time = time.time()
+
+def on_connect(interface):
+    pass
+
+def auto_exit_loop(interface):
+    global last_message_time
+    while not done.is_set():
+        time.sleep(1)
+        if time.time() - last_message_time > stop_timeout:
+            interface.close()
+            done.set()
+
+def main():
+    iface = SerialInterface(serial_port)
+
+    pub.subscribe(handle_text, "meshtastic.receive.text")
+    pub.subscribe(on_connect, "meshtastic.connection.established")
+
+    threading.Thread(target=auto_exit_loop, args=(iface,), daemon=True).start()
+
+    try:
+        done.wait()
+        sys.exit(0)
+    except KeyboardInterrupt:
+        iface.close()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
